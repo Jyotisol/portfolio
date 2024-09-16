2a00b4a962ac1591211a2ab44d0c1c4addd02fb6
jyotisol <jyoti989solanki@gmail.com>
2024-09-16 20:43:02 +0530

    added

--- app/GmailConnect/page.tsx
+++ app/GmailConnect/page.tsx
@@ -1,11 +1,11 @@
 import { Box, ChakraProvider } from "@chakra-ui/react";
-import GmailConnect from "./GmailConnect";
+import ConnectGmail from "./ConnectGmail";
 
 const Page = () => {
   return (
     <ChakraProvider>
       <Box bg="#000319" w="full" h="full" minH="100vh" display="flex" alignItems="center" justifyContent="center">
-      <GmailConnect />
+      <ConnectGmail />
       </Box>
     </ChakraProvider>
   );
